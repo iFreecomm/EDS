edfcc0731bfae9892bf1c0efaed381c5bcbc51bd
hgmwyp <hgmwyp@gmail.com>
2015-03-18 18:20:17 +0800

    update yhz 2

---
+++ js/view/zkhy_ydyhy_add_view.js
@@ -0,0 +1,46 @@
+define(function(require) {
+	var ydyhy_tmpl = require("text!tmpl/zkhy_ydyhy.html");
+	var YdyhyModel = require("model/ydyhy_model");
+	
+	var YhzView = require("view/zkhy_hymb_add_yhz_view");
+	
+	var Backbone = require("backbone");
+	var Handlebars = require("handlebars");
+	
+	
+	var ZkhyYdyhyView = Backbone.View.extend({
+		id: "ydyhy",
+		className: "contentRight",
+		tmpl: Handlebars.compile(ydyhy_tmpl),
+		bindings: {
+			"#hymc": "hymc",
+			"#hyhm": "hyhm",
+			"#zxmm": "zxmm",
+			"#yyrq": "yyrq",
+			"#hymb": "hymb",
+			"#hydk": "hydk",
+			"#hymm": "hymm",
+			"#cxsj": "cxsj"
+		},
+		initialize: function(opt) {
+			var self = this;
+			this.model = new YdyhyModel();
+			if(opt.hyId) {
+				this.model.set({hyId: opt.hyId});
+				this.model.fetch().done(function() {
+					self.render();
+				});
+			} else {
+				this.render();
+			}
+		},
+		render: function() {
+			this.$el.html(this.tmpl(this.model.toJSON()));
+			new YhzView({el: this.$("#select-lxr-box-container")});
+			this.stickit();
+			this.$("select").customSelect();
+		}
+	});
+	
+	return ZkhyYdyhyView;
+});
