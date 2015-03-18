bd46908088831f2c68740c91ac8ab8afbf78721d
hgmwyp <hgmwyp@gmail.com>
2015-03-18 11:32:27 +0800

    update form css

--- js/view/zkhy_hymb_add_basic_view.js
+++ js/view/zkhy_hymb_add_basic_view.js
@@ -5,7 +5,7 @@ define(function(require) {
 	require("customSelect");
 	
 	var BasicView = Backbone.View.extend({
-		className: "tabContent",
+		className: "tabContent active",
 		bindings: {
 			
 		},
