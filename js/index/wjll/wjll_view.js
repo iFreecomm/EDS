b3699726283febd4f676ff9ea0d1ceec4f7d5de5
hgmwyp <hgmwyp@gmail.com>
2015-05-07 11:31:04 +0800

    光盘刻录第一版

--- js/index/wjll/wjll_view.js
+++ js/index/wjll/wjll_view.js
@@ -7,12 +7,15 @@ define(function(require) {
 	var FileView = require("web/index/wjll/file_view");
 	var FileCollection = require("web/index/wjll/file_collection");
 	
+	var BurnDiskView = require("web/index/wjll/burnDisk_view");
+	
 	var WjllView = Mn.LayoutView.extend({
 		id: "wjll_show",
 		template: Handlebars.compile(tmpl),
 		regions: {
 			fileContainer: "#fileContainer",
-			searchTermsContainer: "#searchTermsContainer"
+			searchTermsContainer: "#searchTermsContainer",
+			burnDiskContainer: "#burnDiskContainer"
 		},
 		
 		events: {
@@ -25,6 +28,15 @@ define(function(require) {
 		
 		burnDisk: function() {
 			var idArr = Radio.channel("fileList").request("getSelectedFiles");
+			if(_.isEmpty(idArr)) {
+				alert("请至少选择一个文件后刻录！");
+				return;
+			}
+			if(!this.burnDiskView) {
+				this.burnDiskView = new BurnDiskView();
+				this.showChildView("burnDiskContainer", this.burnDiskView);
+			}
+			this.burnDiskView.show();
 		},
 		
 		batchDelete: function() {
