37a1f2f16d2897e2f62be95c7f6f3feb712df30e
hgmwyp <hgmwyp@gmail.com>
2015-05-21 10:47:01 +0800

    完成整体布局和静音闭音功能

--- js/index/pz/yppz/yppz_route.js
+++ js/index/pz/yppz/yppz_route.js
@@ -4,28 +4,17 @@ define(function(require) {
 	var NavLeftView = require("web/index/pz/navLeft/navLeft_view");
 	var YppzView = require("web/index/pz/yppz/yppz_view");
 	
-	var BdsrModel = require("web/index/pz/yppz/bdsr/bdsr_model");
-	var BdsrView = require("web/index/pz/yppz/bdsr/bdsr_view");
-
 	var YppzRoute = Route.extend({
 		
 		initialize: function(options) {
-			var self = this;
 			this.container = options.container;
-			
-			this.bdsrModel = new BdsrModel();
-			this.bdsrModel.fetch().done(function() {
-				self.showView();
-			});
+			this.showView();
 		},
 		
 		showView: function() {
 			this.show({
 				navLeftView: NavLeftView,
-				contentRightView: new YppzView(),
-				pzYppzBdsrView: new BdsrView({
-					model: this.bdsrModel
-				})
+				contentRightView: new YppzView()
 			});
 		}
 	});
