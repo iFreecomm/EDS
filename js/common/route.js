e4fcd81710e333cf84efb1d22bafd011a6af8f51
hgmwyp <hgmwyp@gmail.com>
2015-06-12 19:40:50 +0800

    修改弹出框样式

--- js/common/route.js
+++ js/common/route.js
@@ -1,4 +1,5 @@
 define(function(require) {
+	var _ = require("underscore");
 	var Mn = require("marionette");
 	var IndexView = require("web/index/index_view");
 	
@@ -6,13 +7,22 @@ define(function(require) {
   		
 		show: function(options) {
 			var indexView = this.container.currentView, navLeftRegion, navLeftView, contentRightRegion;
+			var optNavLeftView = options.navLeftView;
 			if(indexView && indexView instanceof IndexView) {
 				navLeftRegion = indexView.getRegion("navLeft");
 				navLeftView = navLeftRegion.currentView;
 				contentRightRegion = indexView.getRegion("contentRight");
-				if(!(navLeftView && options.navLeftView && navLeftView instanceof options.navLeftView)) {
-					options.navLeftView && navLeftRegion.show(new options.navLeftView()) || navLeftRegion.empty();
+				
+				if(navLeftView && !optNavLeftView) {
+					navLeftRegion.empty();
+				} else if(optNavLeftView) {
+					if(_.isFunction(optNavLeftView) && !(navLeftView instanceof optNavLeftView)) {
+						navLeftRegion.show(new optNavLeftView());
+					} else {
+						navLeftRegion.show(optNavLeftView);
+					}
 				}
+				
 				contentRightRegion.show(options.contentRightView, options);
 			} else {
 				this.container.show(new IndexView(), options);
