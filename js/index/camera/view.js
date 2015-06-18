6b0b5b16c09f3c8b74ec10dd3b42c8730c149e74
hgmwyp <hgmwyp@gmail.com>
2015-06-18 22:06:09 +0800

    摄像机修改控制方法，图片大小

--- js/index/camera/view.js
+++ js/index/camera/view.js
@@ -34,6 +34,9 @@ define(function(require) {
 		},
 		onAttach: function() {
 			Util.activeLink();
+			this.ui.activeBtn.addClass("disabled");
+			this.ui.saveBtn.addClass("disabled");
+			this.ui.cancelBtn.addClass("disabled");
 		},
 		
 		selectLi: function(e) {
@@ -47,11 +50,14 @@ define(function(require) {
 			
 			if($li.is(".using")) {
 				this.ui.cancelBtn.removeClass("disabled");
+				this.ui.saveBtn.removeClass("disabled");
 				this.ui.activeBtn.addClass("disabled");
 			} else if($li.is(".unused")) {
+				this.ui.saveBtn.removeClass("disabled");
 				this.ui.activeBtn.addClass("disabled");
 				this.ui.cancelBtn.addClass("disabled");
 			} else if($li.is(".used")) {
+				this.ui.saveBtn.removeClass("disabled");
 				this.ui.cancelBtn.removeClass("disabled");
 				this.ui.activeBtn.removeClass("disabled");
 			}
@@ -91,7 +97,7 @@ define(function(require) {
 				if(res.code === 0) {
 					var $li = self.ui.lis.eq(index);
 					if($li.is(".unused")) {
-						$li.removeClass("unused").addClass("used").append('<img src="img/camera/cover.png" width="224" height="119" alt="cover"/>');
+						$li.removeClass("unused").addClass("used").append('<img src="img/camera/cover.png" alt="cover"/>');
 					}
 				}
 			});
@@ -120,27 +126,26 @@ define(function(require) {
 				$btn = $btn.parents("button");
 			}
 			
+			var self = this;
+			var vidInPort = this.options.cameraId;
+			var ctrlType = $btn.data("ctrltype");
+			
 			$btn.one("mouseup mouseleave", _.bind(this.endControlCamera, this));
 			
-			this.cameraAct = 0; //开始控制摄像机
-			this.ctrlType = $btn.data("ctrltype");
+			_start();
 			
-			$.getJSON("cameraPTZF.psp", Util.encode({
-				vidInPort: this.options.cameraId,
-				ctrlType: this.ctrlType,
-				cameraAct: this.cameraAct
-			}));
+			function _start() {
+				$.getJSON("cameraPTZF.psp", Util.encode({
+					vidInPort: vidInPort,
+					ctrlType: ctrlType,
+					cameraAct: 0
+				}));
+				self.timerId = setTimeout(_start, 1000);
+			}
 		},
 		
 		endControlCamera: function() {
-			if(this.cameraAct !== 0) return;
-			this.cameraAct = 1; //结束控制摄像机
-			
-			$.getJSON("cameraPTZF.psp", Util.encode({
-				vidInPort: this.options.cameraId,
-				ctrlType: this.ctrlType,
-				cameraAct: this.cameraAct
-			}));
+			clearTimeout(this.timerId);
 		}
 	});
 	
